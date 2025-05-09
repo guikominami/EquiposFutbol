8faed7e50c3d49dc23fb0a93c957270488a9abc4
guikominami <contato.mudalab@gmail.com>
2025-05-09 23:57:11 +0200

    backend external api get teams and countries, favorite teams

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
+const Joi = require('joi');
 const axios = require('axios');
 const express = require('express');
 const app = express();
+
 const config = require('config');
+
+app.use(express.json());
+
 const privateKey = config.get('privateKey');
 
 app.get('/countries', function (req, res) {
@@ -43,4 +48,35 @@ app.get('/teams', function (req, res) {
     });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+const favoriteTeams = [
+  { id: 1, name: 'Barcelona' },
+  { id: 2, name: 'Palmeiras' },
+];
+
+app.get('/favoriteTeams', (req, res) => {
+  res.send(favoriteTeams);
+});
+
+app.post('/favoriteTeams', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const newFavoriteTeam = {
+    id: favoriteTeams.length + 1,
+    name: req.body.name,
+  };
+  favoriteTeams.push(newFavoriteTeam);
+  res.send(newFavoriteTeam);
+});
+
+app.get('/nextMatches', function (req, res) {
+  console.log('next matches por time');
+});
+
+const port = process.env.PORT | 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
