b462b57e9dad5cc98c120000b3bdac617e79c054
guikominami <contato.mudalab@gmail.com>
2025-05-10 19:12:49 +0200

    cors

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -3,11 +3,13 @@ const teams = require('../routes/teams');
 const matches = require('../routes/matches');
 const favoriteTeams = require('../routes/favoriteTeams');
 const users = require('../routes/users');
+const cors = require('../middleware/cors');
 
 const express = require('express');
 
 module.exports = function (app) {
   app.use(express.json());
+  app.use(cors);
   //Routes
   app.use('/api/countries', countries);
   app.use('/api/teams', teams);
