0ba8c923045d1d373de469ee026bf1ea5b008dbe
vermislab vermislab <guilhermevermislab@gmail.com>
2025-05-14 17:44:34 +0200

    connection with credentials in mongo db

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -4,8 +4,10 @@ const config = require('config');
 module.exports = function () {
   mongoose;
   const db = config.get('db');
+  console.log(process.env.db);
+
   mongoose
-    .connect(db)
+    .connect(db, { useNewUrlParser: true })
     .then(() => console.log(`Connected to ${db}...`))
     .catch((err) => console.log('Could not connect to MongoDB...', err));
 };
