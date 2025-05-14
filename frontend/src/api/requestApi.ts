33bcf41b9a22aa92782767ff33a9ded35c8255e0
guikominami <contato.mudalab@gmail.com>
2025-05-14 07:46:16 +0200

    funcao unica para chamada de api

---
+++ frontend/src/api/requestApi.ts
@@ -0,0 +1,27 @@
+export async function requestData(url: string, searchTerm?: string) {
+  if (searchTerm) {
+    url += searchTerm;
+  }
+
+  console.log(url);
+
+  try {
+    // const response = await fetch(url, { signal: signal });
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      const error = new Error(
+        'An error occurred while fetching the user favorite teams.'
+      );
+      error.message = await response.json();
+      throw error;
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    return data.response;
+  } catch (error) {
+    console.error('Error fetching data', error);
+  }
+}
--- frontend/src/api/teams.ts
+++ frontend/src/api/teams.ts
@@ -1,82 +1,109 @@
 import { QueryClient } from '@tanstack/react-query';
 
 import type { TeamFull, FavoriteTeam, NewFavoriteTeam } from '../models/models';
+import { requestData } from './requestApi';
 
 export const queryClient = new QueryClient();
 
 export async function fetchTeams(
-  signal: unknown,
   searchTerm: string,
   searchType: string
 ): Promise<TeamFull[] | undefined> {
-  let teams: TeamFull[] = [];
+  const url = 'http://localhost:3000/api/teams/' + searchType;
 
-  let url = 'http://localhost:3000/api/teams/' + searchType;
-
-  if (searchTerm) {
-    url += searchTerm;
-  }
-
-  try {
-    const response = await fetch(url, { signal: signal });
-
-    if (!response.ok) {
-      const error = new Error('An error occurred while fetching the teams.');
-      error.message = await response.json();
-      throw error;
-    }
-
-    const data = await response.json();
-
-    if (data.errors.length > 0) {
-      const error = new Error('Não foi possível encontrar times neste país.');
-      console.log(data.errors);
-      throw error;
-    }
-
-    teams = data.response;
-
-    return teams;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = requestData(url, searchTerm);
+  const data = response;
+  console.log(data);
+  return data;
 }
 
-export async function fetchFavoriteTeams(userId: string) {
-  let favoriteTeams: FavoriteTeam[] = [];
+export async function fetchFavoriteTeams(
+  searchTerm: string
+): Promise<FavoriteTeam[] | undefined> {
+  console.log('searchTerm', searchTerm);
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+  const url = 'http://localhost:3000/api/favoriteTeams/';
 
-  try {
-    const response = await fetch(
-      'http://localhost:3000/api/favoriteTeams/' + userId,
-      options
-    );
+  console.log('url', url);
 
-    console.log(response);
-
-    if (!response.ok) {
-      const error = new Error(
-        'An error occurred while fetching the user favorite teams.'
-      );
-      error.message = await response.json();
-      throw error;
-    }
-
-    const data = await response.json();
-
-    favoriteTeams = data;
-  } catch (error) {
-    console.log(error);
-  }
-
-  return favoriteTeams;
+  const data = requestData(url, searchTerm);
+  console.log(data);
+  return data;
 }
+
+// export async function fetchTeams(
+//   signal: unknown,
+//   searchTerm: string,
+//   searchType: string
+// ): Promise<TeamFull[] | undefined> {
+//   let teams: TeamFull[] = [];
+
+//   let url = 'http://localhost:3000/api/teams/' + searchType;
+
+//   if (searchTerm) {
+//     url += searchTerm;
+//   }
+
+//   try {
+//     const response = await fetch(url, { signal: signal });
+
+//     if (!response.ok) {
+//       const error = new Error(
+//         'An error occurred while fetching the user favorite teams.'
+//       );
+//       error.message = await response.json();
+//       throw error;
+//     }
+
+//     const data = await response.json();
+
+//     if (data.errors.length > 0) {
+//       const error = new Error('Não foi possível encontrar times neste país.');
+//       console.log(data.errors);
+//       throw error;
+//     }
+
+//     teams = data.response;
+
+//     return teams;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// export async function fetchFavoriteTeams(userId: string) {
+//   let favoriteTeams: FavoriteTeam[] = [];
+
+//   const options = {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   };
+
+//   try {
+//     const response = await fetch(
+//       'http://localhost:3000/api/favoriteTeams/' + userId,
+//       options
+//     );
+
+//     if (!response.ok) {
+//       const error = new Error(
+//         'An error occurred while fetching the user favorite teams.'
+//       );
+//       error.message = await response.json();
+//       throw error;
+//     }
+
+//     const data = await response.json();
+
+//     favoriteTeams = data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+//   return favoriteTeams;
+// }
 
 export async function createNewFavoriteTeam(favoriteTeamData: NewFavoriteTeam) {
   const response = await fetch('http://localhost:3000/api/favoriteTeams ', {
