968b489ab048f42ef17a0a1915d621e795a9b24e
guikominami <contato.mudalab@gmail.com>
2025-05-14 09:02:04 +0200

    eventos por time

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -2,8 +2,12 @@ const { Event } = require('../models/event');
 
 const axios = require('axios');
 const express = require('express');
+const config = require('config');
 const router = express.Router();
 
+const privateKey = config.get('privateKey-externalapi');
+
+//MOCK
 router.get('/:teamName', async (req, res) => {
   const teamName = req.params.teamName;
 
@@ -20,12 +24,28 @@ router.get('/:teamName', async (req, res) => {
   res.send(event);
 });
 
-router.get('/', async (req, res) => {
-  const event = await Event.find();
-  res.send(event);
+router.get('/next/:teamid', function (req, res) {
+  const url = `https://v3.football.api-sports.io/fixtures?season=2023&team=${req.params.teamid}`;
+
+  axios({
+    method: 'get',
+    url,
+    headers: {
+      'x-rapidapi-key': privateKey,
+      'x-rapidapi-host': 'v3.football.api-sports.io',
+    },
+  })
+    .then(function (response) {
+      res.send(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
-router.get('/external/', function (req, res) {
+module.exports = router;
+
+router.get('/liveall/teamid', function (req, res) {
   const url = 'https://v3.football.api-sports.io/fixtures?live=all';
 
   axios({
